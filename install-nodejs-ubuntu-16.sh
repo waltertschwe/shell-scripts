705c00fa084539804c3ec46a934abfffe58d180e
Walter Schweitzer <wschweitzer00@gmail.com>
2017-02-09 02:16:06 +0000

    add nginx

--- install-nodejs-ubuntu-16.sh
+++ install-nodejs-ubuntu-16.sh
@@ -1,6 +1,7 @@
 sudo apt-get install python-software-properties
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install nodejs
+sudo apt-get install nginx 
 
 # node -v
 # npm -v
